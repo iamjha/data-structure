64e94310d17ad1f51620f1e0ed92d9a635dd6e7b
kaminey <vaibhavjha837@gmail.com>
2017-10-03 02:03:48 +0530

    leet code:83 - remove duplicates from sorted lists

---
+++ linked_lists/leet_code_83.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {
+        return head
+    } else {
+        var temp *ListNode = head
+        for temp.Next != nil {
+            if temp.Val == temp.Next.Val {
+                temp.Next = temp.Next.Next
+            } else {
+                temp = temp.Next
+            }
+        }
+    }
+    return head
+    
+}
